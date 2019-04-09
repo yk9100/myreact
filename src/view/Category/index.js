5384bc563872276304ba834a42962a9778319504
yk <437033504@qq.com>
2019-04-09 18:48:45 +0800

    18.48

--- src/view/Category/index.js
+++ src/view/Category/index.js
@@ -2,8 +2,11 @@ import React,{Component} from 'react';
 import style from './index.module.scss';
 class Category extends Component {
     render () {
-        return <div>
-            category
+        return <div id={style.category}>
+            <header>
+                <img src="https://m.wowdsgn.com/static/img/searchIcon-e103f6f03f8488bbc144da2ef8684396.png"/>
+                <span></span><input type="text" placeholder="搜索我的尖叫好物"/>
+            </header>
         </div>
     }
 }
