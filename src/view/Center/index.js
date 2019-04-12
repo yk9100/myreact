94dadc96f65d1ae84181849ab952cbc31cace23c
yk <437033504@qq.com>
2019-04-12 10:07:11 +0800

    yk0412 10.06

--- src/view/Center/index.js
+++ src/view/Center/index.js
@@ -3,7 +3,12 @@ import style from './index.module.scss';
 class Center extends Component {
     render () {
         return <div>
-            center
+            center test用户登录
+            <form action="/login" method="post">
+                用户名:<input type="text" name="username"/><br/>
+                密码:<input type="password" name="password"/>
+                <input type="submit" value="登录"/>
+            </form>
         </div>
     }
 }
--- src/view/Home/Furniture/furniture.js
+++ src/view/Home/Furniture/furniture.js
@@ -4,6 +4,7 @@ import React, {
 import Carousels from '../../../component/Carousels/carousels';
 import Swiper from 'swiper/dist/js/swiper.js';
 import 'swiper/dist/css/swiper.min.css';
+import style from './furniture.module.scss'
 
 class Furniture extends Component {
 	constructor(props) {
@@ -11,17 +12,33 @@ class Furniture extends Component {
 
 		this.state = {
 			datalist: [],
+			itemlist: [],
 		}
 	}
 
 	render() {
 		return (
-			<div>
+			<div id={style.furniture}>
 				{
 					this.state.datalist.length?
 					<Carousels mylist={this.state.datalist}></Carousels>
 					:null
 				}
+				<ul>
+				{
+					this.state.itemlist.length?
+					this.state.itemlist.map((item)=>
+						<li key={item.moduleId}>
+							<h2>{item.moduleName}</h2>
+							<h3>{item.moduleDescription}</h3>
+							<div>
+								{
+									
+								}
+							</div>
+						</li>):null
+				}
+				</ul>
 			</div>
 		)
 	}
@@ -30,7 +47,7 @@ class Furniture extends Component {
 		fetch('/v2/page?pageId=1&tabId=10005&currentPage=1&pageSize=8&_=1554944667655')
 			.then(res => res.json())
 			.then(res => {
-				console.log(res.data.modules[0])
+				//console.log(res.data.modules)
 				this.setState({
 					datalist: res.data.modules[0].moduleContent.banners
 				}, () => {
@@ -41,6 +58,14 @@ class Furniture extends Component {
 							el: '.swp1',
 						}
 					})
+
+				})
+				res.data.modules.shift();
+				res.data.modules.shift();
+				this.setState({
+					itemlist: res.data.modules,
+				},()=>{
+					console.log(res.data.modules)
 
 				})
 			})
